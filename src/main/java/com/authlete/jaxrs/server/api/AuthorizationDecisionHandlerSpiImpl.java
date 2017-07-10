dbebf4d53385f0fb2e013a50a91e251900b88f5b
Justin Richer <justin.richer@authlete.com>
2017-07-10 16:08:23 -0400

    rolled back hack around bug in JAXRS library

--- src/main/java/com/authlete/jaxrs/server/api/AuthorizationDecisionHandlerSpiImpl.java
+++ src/main/java/com/authlete/jaxrs/server/api/AuthorizationDecisionHandlerSpiImpl.java
@@ -100,7 +100,7 @@ class AuthorizationDecisionHandlerSpiImpl extends AuthorizationDecisionHandlerSp
         }
 
         // TODO: this should be passing in seconds to the API but we currently need to pass in milliseconds to get the correct behavior
-        mUserAuthenticatedAt = userAuthenticatedAt.getTime(); // / 1000L;
+        mUserAuthenticatedAt = userAuthenticatedAt.getTime() / 1000L;
 
         // The subject (= unique identifier) of the end-user.
         mUserSubject = mUser.getSubject();
