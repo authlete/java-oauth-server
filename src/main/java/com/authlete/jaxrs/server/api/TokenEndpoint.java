701b9a460f5a10ebc4999c6a7ef2cdd73aa3e5f3
Justin Richer <justin.richer@authlete.com>
2018-05-01 14:53:18 -0400

    added processing of certificates at token endpoint

--- src/main/java/com/authlete/jaxrs/server/api/TokenEndpoint.java
+++ src/main/java/com/authlete/jaxrs/server/api/TokenEndpoint.java
@@ -17,14 +17,24 @@
 package com.authlete.jaxrs.server.api;
 
 
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+
+import org.apache.commons.codec.binary.Base64;
+
 import com.authlete.common.api.AuthleteApiFactory;
 import com.authlete.jaxrs.BaseTokenEndpoint;
 
@@ -43,6 +53,10 @@ import com.authlete.jaxrs.BaseTokenEndpoint;
 @Path("/api/token")
 public class TokenEndpoint extends BaseTokenEndpoint
 {
+    
+    // headers for certificate path with proxy-forwarded certificate information; the first entry is the client's certificate itself
+    private String[] clientCertificatePathHeaders = {"X-Ssl-Cert", "X-Ssl-Cert-Chain-1", "X-Ssl-Cert-Chain-2", "X-Ssl-Cert-Chain-3", "X-Ssl-Cert-Chain-4"};
+    
     /**
      * The token endpoint for {@code POST} method.
      *
@@ -71,10 +85,74 @@ public class TokenEndpoint extends BaseTokenEndpoint
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response post(
             @HeaderParam(HttpHeaders.AUTHORIZATION) String authorization,
-            MultivaluedMap<String, String> parameters)
+            MultivaluedMap<String, String> parameters,
+            @Context HttpServletRequest request)
     {
+        String[] clientCertificates = extractClientCertificates(request);
+        
         // Handle the token request.
         return handle(AuthleteApiFactory.getDefaultApi(),
-                new TokenRequestHandlerSpiImpl(), parameters, authorization);
+                new TokenRequestHandlerSpiImpl(), parameters, authorization, clientCertificates);
+    }
+
+    private String[] extractClientCertificates(HttpServletRequest request)
+    {
+        // try to get the certificates from the servlet context directly
+        X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
+        
+        if (certs == null || certs.length == 0)
+        {
+            // we didn't find any certificates in the servlet request, try extracting them from the headers instead
+            List<String> headerCerts = new ArrayList<>();
+            
+            for (String headerName : clientCertificatePathHeaders)
+            {
+                String header = request.getHeader(headerName);
+                if (header != null)
+                {
+                    headerCerts.add(header);
+                }
+            }
+
+            if (headerCerts.isEmpty())
+            {
+                return null;
+            }
+            else
+            {
+                return headerCerts.toArray(new String[] {});
+            }
+        }
+        else 
+        {
+            String[] pemEncoded = new String[certs.length];
+            
+            // used for encoding certificates
+            Base64 base64 = new Base64(Base64.PEM_CHUNK_SIZE, "\n".getBytes());
+            
+            try
+            {
+                for (int i = 0; i < certs.length; i++)
+                {
+                    // encode each certificate in PEM format
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("-----BEGIN CERTIFICATE-----\n");
+                    sb.append(base64.encode(certs[i].getEncoded()));
+                    sb.append("\n-----END CERTIFICATE-----\n");
+
+                    pemEncoded[i] = sb.toString();
+                    
+                }
+            } catch (CertificateEncodingException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return null;
+            }
+            
+            return pemEncoded;
+            
+        }
+        
     }
 }
