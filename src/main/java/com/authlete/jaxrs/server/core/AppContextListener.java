43a130801a483b42530c5c6a624f0d31afc17ff2
Takahiko Kawasaki <taka@authlete.com>
2021-06-28 07:50:32 +0900

    Added the BouncyCastle library.

---
+++ src/main/java/com/authlete/jaxrs/server/core/AppContextListener.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2021 Authlete, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package com.authlete.jaxrs.server.core;
+
+
+import java.security.Provider;
+import java.security.Security;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
+
+
+public class AppContextListener implements ServletContextListener
+{
+    @Override
+    public void contextInitialized(ServletContextEvent context)
+    {
+        // Initialize BouncyCastle library.
+        Provider bc = BouncyCastleProviderSingleton.getInstance();
+        Security.addProvider(bc);
+    }
+
+
+    @Override
+    public void contextDestroyed(ServletContextEvent context)
+    {
+    }
+}
