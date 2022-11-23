0d0f64bfea2b349cc379da7fb525973f6ba4d928
Takahiko Kawasaki <taka@authlete.com>
2022-11-23 21:01:59 +0900

    Add 'pretty' and 'patch' request parameters to /.well-known/openid-configuration

--- src/main/java/com/authlete/jaxrs/server/api/ConfigurationEndpoint.java
+++ src/main/java/com/authlete/jaxrs/server/api/ConfigurationEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Authlete, Inc.
+ * Copyright (C) 2016-2022 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,11 @@ package com.authlete.jaxrs.server.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import com.authlete.common.api.AuthleteApi;
 import com.authlete.common.api.AuthleteApiFactory;
+import com.authlete.common.dto.ServiceConfigurationRequest;
 import com.authlete.jaxrs.BaseConfigurationEndpoint;
 
 
@@ -73,11 +76,66 @@ public class ConfigurationEndpoint extends BaseConfigurationEndpoint
 {
     /**
      * OpenID Provider configuration endpoint.
+     *
+     * <p>
+     * This implementation accepts {@code "pretty"} and {@code "patch"} as
+     * request parameters, but they are not standardized ones. They are
+     * processed just to demonstrate capabilities of Authlete's
+     * {@code /service/configuration} API. Note that the version of Authlete
+     * must be 2.2.36 or greater to use the request parameters.
+     * </p>
+     *
+     * <p>
+     * The value of the {@code patch} request parameter is a <b>JSON Patch</b>
+     * that conforms to <a href="https://www.rfc-editor.org/rfc/rfc6902">RFC
+     * 6902 JavaScript Object Notation (JSON) Patch</a>. API callers can make
+     * the Authlete API modify JSON on Authlete side before it returns the
+     * configuration JSON. Of course, API callers can modify JSON as they like
+     * AFTER they receive a response from the Authlete API, so API callers do
+     * not necessarily need to use the {@code patch} request parameter.
+     * </p>
      */
     @GET
-    public Response get()
+    public Response get(
+            @QueryParam("pretty") String pretty,
+            @QueryParam("patch") String patch
+            )
     {
-        // Handle the configuration request.
-        return handle(AuthleteApiFactory.getDefaultApi());
+        // An AuthleteApi instance to access Authlete APIs.
+        AuthleteApi api = AuthleteApiFactory.getDefaultApi();
+
+        // If either or both of the 'pretty' request parameter
+        // and the 'patch' request parameter are given.
+        if ((pretty != null && !pretty.isEmpty()) ||
+            (patch  != null && !patch .isEmpty()) )
+        {
+            // Call the /service/configuration API with HTTP POST,
+            // which is supported since Authlete 2.2.36.
+            return handle(api, createRequest(pretty, patch));
+        }
+
+        // Call the /service/configuration API with HTTP GET.
+        return handle(api);
+    }
+
+
+    private static ServiceConfigurationRequest createRequest(String pretty, String patch)
+    {
+        return new ServiceConfigurationRequest()
+                .setPretty(determinePretty(pretty))
+                .setPatch(patch);
+    }
+
+
+    private static boolean determinePretty(String pretty)
+    {
+        // If the 'pretty' request parameter is not given.
+        if (pretty == null || pretty.isEmpty())
+        {
+            // The default value of 'pretty' is true.
+            return true;
+        }
+
+        return Boolean.parseBoolean(pretty);
     }
 }
